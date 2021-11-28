9f63cce409c6a90948ae1341dee40aec3a7c9d90
Ajay Fewell <afewell2@illinois.edu>
2021-11-28 17:57:33 -0600

    modified driver to accomodate multiple gateways via threading

--- fetch_and_localize.py
+++ fetch_and_localize.py
@@ -1,12 +1,17 @@
-import os
 import time
 import json
+import threading
 import subprocess
+
+import algos
 
 NO_DATA_READ      = -2
 JSON_END_OBJECT   = -1
 
 delta_read_time   = 5
+rssi_store        = {} # rssi_store[gateway_id (str)] -> [rssi readings (arr of nums)]
+rssi_store_mutex  = threading.Lock()
+
 
 def follow(filename):
     f = open(filename, "r")
@@ -34,6 +39,26 @@ def extract_rssi(json_payload):
     return rssi
 
 
+def listen(gateway_id, output_filename):
+    json_payload = ""
+    rssi         = None
+    for line in follow(output_filename):
+        if line == NO_DATA_READ or line == JSON_END_OBJECT:
+            if json_payload:
+                print(json_payload)
+                rssi = extract_rssi(json_payload)
+            if rssi:
+                rssi_store_mutex.acquire()
+                rssi_store[gateway_id].append(rssi)
+                print(rssi_store)
+                rssi_store_mutex.release()
+            json_payload = ""
+            if line == NO_DATA_READ:
+                time.sleep(delta_read_time)
+        else:
+            json_payload += line
+
+
 if __name__ == "__main__":
     gateway_ids = [
         "afewell-gateway",
@@ -47,39 +72,19 @@ if __name__ == "__main__":
         "output-3"
     ]
 
-    cmd_login = "ttn-lw-cli login"
-    
+    cmd_login  = "ttn-lw-cli login"
     login_proc = subprocess.Popen(cmd_login, shell=True, stdout=subprocess.PIPE)
     login_proc.wait()
-
-    rssi_store = {} # gateway_id {0, 1, 2} -> timestamp/packet num -> rssi reading
     
+    threads = []
     for i in range(3):
         cmd_gateway_traffic = f"ttn-lw-cli events --gateway-id {gateway_ids[i]} > {traffic_filenames[i]}"
         proc = subprocess.Popen([cmd_gateway_traffic], shell=True, stdin=None, stdout=None, stderr=None)
         time.sleep(1)
-
-
-    avg_rssi     = 0
-    num_recv     = 0
-    json_payload = ""
-    rssi         = None
-    for line in follow(traffic_filenames[0]):
-        if line == NO_DATA_READ or line == JSON_END_OBJECT:
-            if json_payload:
-                rssi = extract_rssi(json_payload)
-            if rssi:
-                avg_rssi = (avg_rssi * num_recv + rssi) / (num_recv + 1)
-                num_recv += 1
-                print("received rssi: ", rssi)
-                print("average rssi: ", avg_rssi)
-                rssi = None
-                if num_recv == 20:
-                    break
-            json_payload = ""
-            if line == NO_DATA_READ:
-                time.sleep(delta_read_time)
-        else:
-            json_payload += line
-
-
+        
+        t = threading.Thread(target=listen, args=(gateway_ids[i], traffic_filenames[i], ))
+        t.start()
+        threads.append(t)
+    
+    for t in threads:
+        t.join()
